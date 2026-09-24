71c7a799db26c10ee1c23b8483d95add60f4d047
Ken French <kfrench@localhost.localdomain>
2026-09-23 20:28:37 -0400

    fixing build file

--- monitoring-app/build-image.sh
+++ monitoring-app/build-image.sh
@@ -1,4 +1,4 @@
 mvn clean package
-podman build -f src/main/docker/Dockerfile.jvm -t quarkus/Sensortrain-monitoring-app-jvm --platform linux/arm64/v8 . 
+podman build -f src/main/docker/Dockerfile.jvm -t quarkus/sensortrain-monitoring-app-jvm --platform linux/arm64/v8 . 
 #podman tag quarkus/Sensortrain-monitoring-app-jvm:latest quay.io/mouachan/Sensortrain-monitoring-app-arm64:latest    
 # podman push quay.io/mouachan/Sensortrain-monitoring-app-arm64:latest 
